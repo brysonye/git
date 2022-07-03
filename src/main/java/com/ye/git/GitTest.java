82dd100b690eb795e25a7bcf2907bbbc6a83d98e
Jump <Jump@777.com>
2022-07-03 10:10:49 +0800

    first commit

---
+++ src/main/java/com/ye/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.ye.git;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author : YePengCheng
+ * @create 2022/7/3 10:06
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
